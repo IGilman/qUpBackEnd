4f9d0d402bb81f70eb83481786a0cd6f9489b1e1
isaac <gilman.isaac@gmail.com>
2021-09-09 22:18:09 -0400

    playing around with CORS, likely won't keep this

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const { clientOrigins, serverPort } = require("./app/config/env.dev");
 
 const app = express();
 
-app.use(cors({ origin: clientOrigins }));
+app.use(cors({
+    origin: clientOrigins,
+    // origin: "*",
+    credentials: true,
+    allowedHeaders: "*",
+    exposedHeaders: "test",
+    credentials: true,
+    "Access-Control-Allow-Credentials": true,
+})
+);
 
 const db = require("./app/config/db.config")
 
@@ -31,7 +40,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
+    res.set({
+        "Content-Type": "text/html",
+        "X-Content-Type-Options": "nosniff",
+        "Access-Control-Allow-Credentials": true,
+        'warning': 'testing'
+    })
+
     res.json({ message: "Server.js is running" });
+
+
 });
 
 require("./app/routes/qup.routes")(app);
